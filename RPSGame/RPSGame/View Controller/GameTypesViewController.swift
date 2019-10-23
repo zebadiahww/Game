86ca621ab40b8f9dff5b84594e3ce0bd1d48ef2c
Bethany Wride <bethanywride@Bethanys-MacBook-Air-2.local>
2019-10-23 12:26:55 -0600

    Creates view controller files

---
+++ RPSGame/RPSGame/View Controller/GameTypesViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GameTypesViewController.swift
+//  RPSGame
+//
+//  Created by Bethany Wride on 10/23/19.
+//  Copyright © 2019 Zebadiah Watson. All rights reserved.
+//
+
+import UIKit
+
+class GameTypesViewController: UIViewController {
+// MARK: - Outlets
+    
+// MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+// MARK: - Actions
+    
+
+// MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    }
+}
